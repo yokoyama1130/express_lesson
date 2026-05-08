3daeef7b1fced3335305eab9d2f09f9d58b076a7
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-05-08 22:22:10 +0900

    いらない行の削除

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ const port = 3000;
 // pathというモジュールを取得する
 const path = require("path");
 
-
-
-
 // デフォルトではエクスプレスはviewsファイルを見にいくが、これはどこのディレクトリでサーバーを起動したかに依存する
 // cd .. で一個上のディレクトリから node template/index.js というようにサーバーは起動はできるが、ページを読み込むとエラーが出てしまう
 // どこから実行しても正常に実行して欲しい場合はデフォルトの設定を変える必要がある
@@ -24,9 +21,6 @@ app.set("views", path.join(__dirname, "views"));
 // 第一引数に __dirname という変数を置く（node.jsの特別な変数で今実行してるファイルが存在してるディレクトリへの絶対パスを返してくれる（index.jsが存在してるtemplating_demoまでの絶対パスを返してくれる））
 // 第二引数に views を置く（最後に views というパスを追加）
 
-
-
-
 // ejsを使えるようにする（setはこのファイルの設定みたいなもの）
 // このコードはエクスプレスのドキュメントに書いてあるルール
 app.set("view engine", "ejs");
@@ -35,10 +29,6 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     res.render("home");
 });
-
-
-
-
 
 // サーバー起動
 app.listen(port, () => {
